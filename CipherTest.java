be405af7805b9269bd80a1636b9a59f1d067c481
Cody Machine <codymachine93@gmail.com>
2015-06-20 03:10:54 -0700

    Added lines testing en/decryptArray

--- CipherTest.java
+++ CipherTest.java
@@ -7,7 +7,7 @@ public class CipherTest
 {
    public static void main(String[] args) throws Exception
    {
-      Cryptor c = new Cryptor("password");
+      /*Cryptor c = new Cryptor("password");
       String temp = "This is a test. This is a test.";
       String enc = c.encrypt(temp);
       String dec = c.decrypt(enc);
@@ -24,6 +24,27 @@ public class CipherTest
       System.out.println("String after decryption with correct pass after setKey call: " + temp2);
       
       //Cryptor e = new Cryptor();
+      */
+      
+      Cryptor c = new Cryptor("password");
+      String[] temp = {"##########", "username", "pass", "http://www.blurp.com", "NAME", "PASSWORD", "http://facebook.com", "FBNAME", "duhpass", "http://www.website.com", "mork", "mindy", "http://mail.hotmale.cz", "Timothy", "Treadwell"};
+      temp = c.encryptArray(temp);
+      for (int x = 0; x < temp.length; x++)
+      {
+         System.out.println(temp[x]);
+      }
+      temp = c.decryptArray(temp);
+      for (int x = 0; x < temp.length; x++)
+      {
+         System.out.println(temp[x]);
+      }
+      temp = c.encryptArray(temp);
+      c.setKey("notpass");
+      temp = c.decryptArray(temp);
+      for (int x = 0; x < temp.length; x++)
+      {
+         System.out.println(temp[x]);
+      }
       
    } // end main
    
